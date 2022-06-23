52a0c2a0e43b68ac47018874bccaecf15527a3c1
Maxwell Alvord <maxwellalvord@gmail.com>
2022-06-22 18:39:27 -0700

    file loader and html loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,24 @@ module.exports = {
         ]
       },
       {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
